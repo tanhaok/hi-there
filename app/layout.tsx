051f6a4b019ac31c3b2234607d98643b988dc5ca
halng <haonguyentan2001@gmail.com>
2024-11-06 18:34:45 +0700

    fix: syntax error

--- app/layout.tsx
+++ app/layout.tsx
@@ -24,7 +24,7 @@ export default function RootLayout({
         <script
           async
           src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-4284950481215978"
-          crossorigin="anonymous"
+          crossOrigin="anonymous"
         ></script>
       </head>
       <body>
